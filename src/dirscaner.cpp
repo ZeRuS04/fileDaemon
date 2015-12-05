df765526a5e40394be555010f06672590413fa32
root <root@homeless.homeless.loc>
2015-12-05 14:06:09 +0300

    wip

---
+++ src/dirscaner.cpp
@@ -0,0 +1,7 @@
+#include "dirscaner.h"
+
+DirScaner::DirScaner()
+{
+
+}
+
---
+++ src/dirscaner.h
@@ -0,0 +1,11 @@
+#ifndef DIRSCANER_H
+#define DIRSCANER_H
+
+
+class DirScaner
+{
+public:
+    DirScaner();
+};
+
+#endif // DIRSCANER_H
